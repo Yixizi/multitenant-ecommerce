af53983adf4e9326dce57e21c2557855ccd5c13a
Yixizi <2707633931@qq.com>
2025-04-15 23:10:10 +0800

    01:初始配置

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen  flex justify-center items-center">
+      <p className=" text-rose-500">Hello World</p>
+    </div>
+  );
+}
